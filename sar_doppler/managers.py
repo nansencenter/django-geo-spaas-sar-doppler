65a91d72e74c885e8a938242ce179fbbd27799d2
mortenwh <morten.wergeland.hansen@gmail.com>
2018-03-02 16:03:08 +0100

    commented out code for post-processing

--- sar_doppler/managers.py
+++ sar_doppler/managers.py
@@ -124,194 +124,200 @@ class DatasetManager(DM):
         # Get geolocation of dataset - this must be updated
         geoloc = ds.geographic_location
         # Check geometry, return if it is the same as the stored one
-        if geoloc.geometry == new_geometry and not reprocess:
-            return ds, False
-
+        created = False
         if geoloc.geometry != new_geometry:
             # Change the dataset geolocation to cover all subswaths
             geoloc.geometry = new_geometry
             geoloc.save()
+            created = True
+        
+        return ds, created
 
-        # Create data products
-        mm = self.__module__.split('.')
-        module = '%s.%s' % (mm[0], mm[1])
-        mp = media_path(module, swath_data[i].fileName)
-        ppath = product_path(module, swath_data[i].fileName)
+        """
+        NOTE: this get_or_create should only add the gsar rvl files to the geo-spaas catalog.
+        Another method/function/script should be made for processing into nc-files, and yet another
+        one to ingest the nc-files
+        """
+        ## Create data products
+        #mm = self.__module__.split('.')
+        #module = '%s.%s' % (mm[0], mm[1])
+        #mp = media_path(module, swath_data[i].fileName)
+        #ppath = product_path(module, swath_data[i].fileName)
 
-        for i in range(n_subswaths):
-            is_corrupted = False
-            # Check if the file is corrupted
-            try:
-                inci = swath_data[i]['incidence_angle']
-            #  TODO: What kind of exception ?
-            except:
-                is_corrupted = True
-                continue
+        #for i in range(n_subswaths):
+        #    is_corrupted = False
+        #    # Check if the file is corrupted
+        #    try:
+        #        inci = swath_data[i]['incidence_angle']
+        #    #  TODO: What kind of exception ?
+        #    except:
+        #        is_corrupted = True
+        #        continue
 
-            # Add Doppler anomaly
-            swath_data[i].add_band(array=swath_data[i].anomaly(), parameters={
-                'wkv':
-                'anomaly_of_surface_backwards_doppler_centroid_frequency_shift_of_radar_wave'
-            })
+        #    # Add Doppler anomaly
+        #    swath_data[i].add_band(array=swath_data[i].anomaly(), parameters={
+        #        'wkv':
+        #        'anomaly_of_surface_backwards_doppler_centroid_frequency_shift_of_radar_wave'
+        #    })
 
-            # Find matching NCEP forecast wind field
-            wind = [] # do not do any wind correction now, since we have lookup tables
-            #wind = Dataset.objects.filter(
-            #        source__platform__short_name='NCEP-GFS',
-            #        time_coverage_start__range=[
-            #            parse(swath_data[i].get_metadata()['time_coverage_start'])
-            #            - timedelta(hours=3),
-            #            parse(swath_data[i].get_metadata()['time_coverage_start'])
-            #            + timedelta(hours=3)
-            #        ]
-            #    )
-            band_number = swath_data[i]._get_band_number({
-                'standard_name': 'surface_backwards_doppler_centroid_frequency_shift_of_radar_wave',
-                })
+        #    # Find matching NCEP forecast wind field
+        #    wind = [] # do not do any wind correction now, since we have lookup tables
+        #    #wind = Dataset.objects.filter(
+        #    #        source__platform__short_name='NCEP-GFS',
+        #    #        time_coverage_start__range=[
+        #    #            parse(swath_data[i].get_metadata()['time_coverage_start'])
+        #    #            - timedelta(hours=3),
+        #    #            parse(swath_data[i].get_metadata()['time_coverage_start'])
+        #    #            + timedelta(hours=3)
+        #    #        ]
+        #    #    )
+        #    band_number = swath_data[i]._get_band_number({
+        #        'standard_name': 'surface_backwards_doppler_centroid_frequency_shift_of_radar_wave',
+        #        })
 
-            pol = swath_data[i].get_metadata(bandID=band_number, key='polarization')
+        #    pol = swath_data[i].get_metadata(bandID=band_number, key='polarization')
 
-            if wind:
-                dates = [w.time_coverage_start for w in wind]
+        #    if wind:
+        #        dates = [w.time_coverage_start for w in wind]
 
-                # TODO: Come back later (!!!)
-                nearest_date = min(dates, key=lambda d:
-                        abs(d-parse(swath_data[i].get_metadata()['time_coverage_start']).replace(tzinfo=timezone.utc)))
+        #        # TODO: Come back later (!!!)
+        #        nearest_date = min(dates, key=lambda d:
+        #                abs(d-parse(swath_data[i].get_metadata()['time_coverage_start']).replace(tzinfo=timezone.utc)))
 
-                fww = swath_data[i].wind_waves_doppler(
-                        nansat_filename(wind[dates.index(nearest_date)].dataseturi_set.all()[0].uri),
-                        pol
-                    )
+        #        fww = swath_data[i].wind_waves_doppler(
+        #                nansat_filename(wind[dates.index(nearest_date)].dataseturi_set.all()[0].uri),
+        #                pol
+        #            )
 
-                swath_data[i].add_band(array=fww, parameters={
-                    'wkv':
-                    'surface_backwards_doppler_frequency_shift_of_radar_wave_due_to_wind_waves'
-                })
+        #        swath_data[i].add_band(array=fww, parameters={
+        #            'wkv':
+        #            'surface_backwards_doppler_frequency_shift_of_radar_wave_due_to_wind_waves'
+        #        })
 
-                fdg = swath_data[i].geophysical_doppler_shift(
-                    wind=nansat_filename(wind[dates.index(nearest_date)].dataseturi_set.all()[0].uri)
-                )
+        #        fdg = swath_data[i].geophysical_doppler_shift(
+        #            wind=nansat_filename(wind[dates.index(nearest_date)].dataseturi_set.all()[0].uri)
+        #        )
 
-                # Estimate current by subtracting wind-waves Doppler
-                theta = swath_data[i]['incidence_angle'] * np.pi / 180.
-                vcurrent = -np.pi * (fdg - fww) / (112. * np.sin(theta))
+        #        # Estimate current by subtracting wind-waves Doppler
+        #        theta = swath_data[i]['incidence_angle'] * np.pi / 180.
+        #        vcurrent = -np.pi * (fdg - fww) / (112. * np.sin(theta))
 
-                # Smooth...
-                # vcurrent = median_filter(vcurrent, size=(3,3))
-                swath_data[i].add_band(
-                    array=vcurrent,
-                    parameters={
-                        'wkv': 'surface_radial_doppler_sea_water_velocity'
-                    })
-            else:
-                fww = None
-                fdg = swath_data[i].geophysical_doppler_shift()
+        #        # Smooth...
+        #        # vcurrent = median_filter(vcurrent, size=(3,3))
+        #        swath_data[i].add_band(
+        #            array=vcurrent,
+        #            parameters={
+        #                'wkv': 'surface_radial_doppler_sea_water_velocity'
+        #            })
+        #    else:
+        #        fww = None
+        #        fdg = swath_data[i].geophysical_doppler_shift()
 
-            swath_data[i].add_band(
-                array=fdg,
-                parameters={
-                    'wkv': 'surface_backwards_doppler_frequency_shift_of_radar_wave_due_to_surface_velocity'
-                })
+        #    swath_data[i].add_band(
+        #        array=fdg,
+        #        parameters={
+        #            'wkv': 'surface_backwards_doppler_frequency_shift_of_radar_wave_due_to_surface_velocity'
+        #        })
 
-            # Export data to netcdf
-            print('Exporting %s (subswath %d)' % (swath_data[i].fileName, i))
+        #    # Export data to netcdf
+        #    print('Exporting %s (subswath %d)' % (swath_data[i].fileName, i))
 
-            fn = os.path.join(ppath,
-                              os.path.basename(swath_data[i].fileName).split('.')[0]
-                              + 'subswath%d.nc' % i)
+        #    fn = os.path.join(ppath,
+        #                      os.path.basename(swath_data[i].fileName).split('.')[0]
+        #                      + 'subswath%d.nc' % i)
 
-            origFile = swath_data[i].fileName
+        #    origFile = swath_data[i].fileName
 
-            try:
-                swath_data[i].set_metadata(key='Originating file',
-                                           value=origFile)
-            except Exception as e:
-                # TODO: Should it be here?
-                warnings.warn('%s: BUG IN GDAL(?) - SHOULD BE CHECKED..' % e.message)
+        #    try:
+        #        swath_data[i].set_metadata(key='Originating file',
+        #                                   value=origFile)
+        #    except Exception as e:
+        #        # TODO: Should it be here?
+        #        warnings.warn('%s: BUG IN GDAL(?) - SHOULD BE CHECKED..' % e.message)
 
-            swath_data[i].export(fileName=fn)
-            ncuri = os.path.join('file://localhost', fn)
-            new_uri, created = DatasetURI.objects.get_or_create(uri=ncuri,
-                                                                dataset=ds)
+        #    swath_data[i].export(fileName=fn)
+        #    ncuri = os.path.join('file://localhost', fn)
+        #    new_uri, created = DatasetURI.objects.get_or_create(uri=ncuri,
+        #                                                        dataset=ds)
 
-            # Maybe add figures in satellite projection...
-            # filename = 'satproj_fdg_subswath_%d.png'%i
-            # swath_data[i].write_figure(os.path.join(mp, filename),
-            #        bands='fdg', clim=[-60,60], cmapName='jet')
+        #    # Maybe add figures in satellite projection...
+        #    # filename = 'satproj_fdg_subswath_%d.png'%i
+        #    # swath_data[i].write_figure(os.path.join(mp, filename),
+        #    #        bands='fdg', clim=[-60,60], cmapName='jet')
 
-            #  Add figure to db
-            
-            # Reproject to leaflet projection
-            xlon, xlat = swath_data[i].get_corners()
-            d = Domain(NSR(3857),
-                       '-lle %f %f %f %f -tr 1000 1000'
-                       % (xlon.min(), xlat.min(), xlon.max(), xlat.max()))
+        #    #  Add figure to db
+        #    
+        #    # Reproject to leaflet projection
+        #    xlon, xlat = swath_data[i].get_corners()
+        #    d = Domain(NSR(3857),
+        #               '-lle %f %f %f %f -tr 1000 1000'
+        #               % (xlon.min(), xlat.min(), xlon.max(), xlat.max()))
 
-            swath_data[i].reproject(d, eResampleAlg=1, tps=True)
+        #    swath_data[i].reproject(d, eResampleAlg=1, tps=True)
 
-            # Check if the reprojection failed
-            try:
-                inci = swath_data[i]['incidence_angle']
+        #    # Check if the reprojection failed
+        #    try:
+        #        inci = swath_data[i]['incidence_angle']
 
-            # TODO: What kind of exception?
-            except:
-                is_corrupted = True
-                warnings.warn('Could not read incidence angles - reprojection failed')
-                continue
+        #    # TODO: What kind of exception?
+        #    except:
+        #        is_corrupted = True
+        #        warnings.warn('Could not read incidence angles - reprojection failed')
+        #        continue
 
-            # Visualizations of the following bands (short_names) are created
-            # when ingesting data:
-            ingest_creates = ['valid_doppler',
-                              'valid_land_doppler',
-                              'valid_sea_doppler',
-                              'dca',
-                              'fdg']
-            if wind:
-                ingest_creates.extend(['fww', 'Ur'])
+        #    # Visualizations of the following bands (short_names) are created
+        #    # when ingesting data:
+        #    ingest_creates = ['valid_doppler',
+        #                      'valid_land_doppler',
+        #                      'valid_sea_doppler',
+        #                      'dca',
+        #                      'fdg']
+        #    if wind:
+        #        ingest_creates.extend(['fww', 'Ur'])
 
-            # (the geophysical doppler shift must later be added in a separate
-            # manager method in order to estimate the range bias after
-            # processing multiple files)
-            for band in ingest_creates:
-                filename = '%s_subswath_%d.png' % (band, i)
-                # check uniqueness of parameter
-                param = Parameter.objects.get(short_name=band)
-                fig = swath_data[i].write_figure(
-                    os.path.join(mp, filename),
-                    bands=band,
-                    mask_array=swath_data[i]['swathmask'],
-                    mask_lut={0: [128, 128, 128]},
-                    transparency=[128, 128, 128])
+        #    # (the geophysical doppler shift must later be added in a separate
+        #    # manager method in order to estimate the range bias after
+        #    # processing multiple files)
+        #    for band in ingest_creates:
+        #        filename = '%s_subswath_%d.png' % (band, i)
+        #        # check uniqueness of parameter
+        #        param = Parameter.objects.get(short_name=band)
+        #        fig = swath_data[i].write_figure(
+        #            os.path.join(mp, filename),
+        #            bands=band,
+        #            mask_array=swath_data[i]['swathmask'],
+        #            mask_lut={0: [128, 128, 128]},
+        #            transparency=[128, 128, 128])
 
-                if type(fig) == Figure:
-                    print 'Created figure of subswath %d, band %s' % (i, band)
-                else:
-                    warnings.warn('Figure NOT CREATED')
+        #        if type(fig) == Figure:
+        #            print 'Created figure of subswath %d, band %s' % (i, band)
+        #        else:
+        #            warnings.warn('Figure NOT CREATED')
 
-                # Get DatasetParameter
-                dsp, created = DatasetParameter.objects.get_or_create(dataset=ds,
-                                                                      parameter=param)
+        #        # Get DatasetParameter
+        #        dsp, created = DatasetParameter.objects.get_or_create(dataset=ds,
+        #                                                              parameter=param)
 
-                # Create Visualization
-                try:
-                    geom, created = GeographicLocation.objects.get_or_create(
-                        geometry=WKTReader().read(swath_data[i].get_border_wkt()))
-                except Exception as inst:
-                    print(type(inst))
-                    import ipdb
-                    ipdb.set_trace()
-                    raise
+        #        # Create Visualization
+        #        try:
+        #            geom, created = GeographicLocation.objects.get_or_create(
+        #                geometry=WKTReader().read(swath_data[i].get_border_wkt()))
+        #        except Exception as inst:
+        #            print(type(inst))
+        #            import ipdb
+        #            ipdb.set_trace()
+        #            raise
 
-                vv, created = Visualization.objects.get_or_create(
-                    uri='file://localhost%s/%s' % (mp, filename),
-                    title='%s (swath %d)' % (param.standard_name, i + 1),
-                    geographic_location=geom
-                )
+        #        vv, created = Visualization.objects.get_or_create(
+        #            uri='file://localhost%s/%s' % (mp, filename),
+        #            title='%s (swath %d)' % (param.standard_name, i + 1),
+        #            geographic_location=geom
+        #        )
 
-                # Create VisualizationParameter
-                vp, created = VisualizationParameter.objects.get_or_create(
-                    visualization=vv,
-                    ds_parameter=dsp
-                )
+        #        # Create VisualizationParameter
+        #        vp, created = VisualizationParameter.objects.get_or_create(
+        #            visualization=vv,
+        #            ds_parameter=dsp
+        #        )
 
-        return ds, not is_corrupted
+        #return ds, not is_corrupted
