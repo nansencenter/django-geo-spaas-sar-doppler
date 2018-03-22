2bc6b0eab350fd9623db5f009789cc5d385c4ed0
mortenwh <morten.wergeland.hansen@gmail.com>
2018-03-22 11:56:05 +0100

    made new command process_ingested_sar_doppler to process all ingested gsar files that have not already been processed. Made a test that is not complete - we need to return to that...

---
+++ sar_doppler/management/commands/process_ingested_sar_doppler.py
@@ -0,0 +1,29 @@
+''' Processing of SAR Doppler from Norut's GSAR '''
+from django.core.management.base import BaseCommand
+
+from sar_doppler.models import Dataset
+
+class Command(BaseCommand):
+    help = 'Post-processing of ingested GSAR RVL files and generation of png images for ' \
+            'display in Leaflet'
+
+    # Reprocessing should probably be a separate command
+    #def add_arguments(self, parser):
+    #    parser.add_argument('--reprocess', action='store_true', 
+    #            help='Force reprocessing')
+
+    def handle(self, *args, **options):
+        unprocessed = Dataset.objects.filter(
+                entry_title='SAR Doppler').exclude(dataseturi__uri__endswith='.nc')
+        num_unprocessed = len(unprocessed)
+
+        for i,ds in enumerate(unprocessed):
+            uri = ds.dataseturi_set.get(uri__endswith='.gsar').uri
+            updated_ds, corrupted = Dataset.objects.process(uri)
+            if not corrupted:
+                self.stdout.write('Successfully processed (%d/%d): %s\n' % (i+1, num_unprocessed,
+                    uri))
+            else:
+                self.stdout.write('Corrupt file (%d/%d, may have been partly processed): %s\n' %(i,
+                    num_unprocessed, uri))
+
--- sar_doppler/tests.py
+++ sar_doppler/tests.py
@@ -1,18 +1,34 @@
+from mock import patch, Mock, DEFAULT
+
 from django.test import TestCase
 from django.core.management import call_command
 from django.utils.six import StringIO
+
+from sar_doppler.models import Dataset
+from sar_doppler.managers import DatasetManager
 
 class TestProcessingSARDoppler(TestCase):
 
     fixtures = ["vocabularies"]
 
-    def test_process_sar_doppler(self):
-        out = StringIO()
-        wf = 'file://localhost/mnt/10.11.12.231/sat_auxdata/model/ncep/gfs/' \
-                'gfs20091116/gfs.t18z.master.grbf03'
-        call_command('ingest', wf, stdout=out)
-        f = 'file://localhost/mnt/10.11.12.231/sat_downloads_asar/level-0/' \
-                'gsar_rvl/RVL_ASA_WS_20091116195940116.gsar'
-        call_command('ingest_sar_doppler', f, stdout=out)
-        self.assertIn('Successfully added:', out.getvalue())
+    #def test_process_sar_doppler(self):
+    #    out = StringIO()
+    #    wf = 'file://localhost/mnt/10.11.12.231/sat_auxdata/model/ncep/gfs/' \
+    #            'gfs20091116/gfs.t18z.master.grbf03'
+    #    call_command('ingest', wf, stdout=out)
+    #    f = 'file://localhost/mnt/10.11.12.231/sat_downloads_asar/level-0/' \
+    #            'gsar_rvl/RVL_ASA_WS_20091116195940116.gsar'
+    #    call_command('ingest_sar_doppler', f, stdout=out)
+    #    self.assertIn('Successfully added:', out.getvalue())
 
+    @patch.multiple(DatasetManager, filter=DEFAULT, process=DEFAULT, exclude=Mock(return_value=None))
+    def test_process_ingested_sar_doppler(self, filter, process):
+        #mock_ds_objects.filter.return_value = mock_ds_objects
+        #mock_ds_objects.exclude.return_value = mock_ds_objects
+        #mock_ds_objects.process.return_value = (mock_ds_objects, True)
+
+        out = StringIO()
+        call_command('process_ingested_sar_doppler', stdout=out)
+        filter.assert_called()
+        #exclude.assert_called_once()
+        process.assert_called_once()
