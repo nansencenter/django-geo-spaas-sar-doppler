1661b2b5ce3cc05ee61bfcd51e7c6093bcf27739
Artem Moiseev <korvinos@bk.ru>
2018-07-10 14:15:01 +0200

    Added new commands

--- sar_doppler/management/commands/ingest_sar_doppler.py
+++ sar_doppler/management/commands/ingest_sar_doppler.py
@@ -16,18 +16,55 @@ import os
 
 class Command(BaseCommand):
     args = '<filename>'
-    help = 'Add WS file to catalog archive and make png images for ' \
-            'display in Leaflet'
+    # TODO: The description should be updated with new functionality and examples. Artem. 10-07-18
+    help = 'Add WS file to catalog archive and make png images for display in Leaflet'
 
     def add_arguments(self, parser):
         parser.add_argument('gsar_files', nargs='*', type=str)
-        parser.add_argument('--reprocess', action='store_true', 
-                help='Force reprocessing')
+
+        # Spatial reference options
+        parser.add_argument('--epsg',
+                            metavar='EPSG_CODE',
+                            type=int,
+                            help='Coordinate systems (projected or geographic) can be '
+                                 'selected based on their EPSG codes')
+        parser.add_argument('--proj4',
+                            metavar='\'PROJ4 STRING\'',
+                            type=str,
+                            help='A PROJ.4 definition string can be used as a coordinate'
+                                 ' system. For instance "+proj=utm +zone=11 +datum=WGS84".')
+        # Extent options
+        parser.add_argument('--tr',
+                            metavar='\'X Y\'',
+                            type=str,
+                            help='Set target resolution. The values must be expressed in '
+                                 'georeferenced units. Both must be positive values')
+        parser.add_argument('--te',
+                            metavar='\'X_MIN Y_MIN X_MAX Y_MAX\'',
+                            type=str,
+                            help='Set georeferenced extents. The values must be expressed '
+                                 'in georeferenced units. If not specified, the extent of '
+                                 'the output file will be the extent of the vector layers.')
+        parser.add_argument('--ts',
+                            metavar='\'WIDTH HEIGHT\'',
+                            type=str,
+                            help='Set output file size in pixels and lines. '
+                                 'Note that --ts cannot be used with --tr. size_x size_y')
+        parser.add_argument('--lle',
+                            metavar='\'LON_MIN LAT_MIN LON_MAX LAT_MAX\'',
+                            type=str,
+                            help='Set domain boundaries. '
+                                 'Note that --lle cannot be used with --te')
+
+        # Processing options and features
+        parser.add_argument('--reprocess',
+                            action='store_true',
+                            help='Force reprocessing')
 
     def handle(self, *args, **options):
         #if not len(args)==1:
         #    raise IOError('Please provide one filename only')
-
+        print(options)
         for non_ingested_uri in uris_from_args(options['gsar_files']):
             self.stdout.write('Ingesting %s ...\n' % non_ingested_uri)
             ds, cr = Dataset.objects.get_or_create(non_ingested_uri, **options)
--- sar_doppler/tests.py
+++ sar_doppler/tests.py
@@ -1,18 +1,32 @@
 from django.test import TestCase
 from django.core.management import call_command
 from django.utils.six import StringIO
+import sys
 
-class TestProcessingSARDoppler(TestCase):
 
+# class TestProcessingSARDoppler(TestCase):
+#
+#    fixtures = ["vocabularies"]
+#
+#    def test_process_sar_doppler(self):
+#        out = StringIO()
+#        wf = 'file://localhost/mnt/10.11.12.231/sat_auxdata/model/ncep/gfs/' \
+#                'gfs20091116/gfs.t18z.master.grbf03'
+#        call_command('ingest', wf, stdout=out)
+#        f = 'file://localhost/mnt/10.11.12.231/sat_downloads_asar/level-0/' \
+#                'gsar_rvl/RVL_ASA_WS_20091116195940116.gsar'
+#        call_command('ingest_sar_doppler', f, stdout=out)
+#        self.assertIn('Successfully added:', out.getvalue())
+
+
+class TestIngestSARDopplerCommand(TestCase):
     fixtures = ["vocabularies"]
 
-    def test_process_sar_doppler(self):
+    def test_command_options(self):
         out = StringIO()
-        wf = 'file://localhost/mnt/10.11.12.231/sat_auxdata/model/ncep/gfs/' \
-                'gfs20091116/gfs.t18z.master.grbf03'
-        call_command('ingest', wf, stdout=out)
-        f = 'file://localhost/mnt/10.11.12.231/sat_downloads_asar/level-0/' \
-                'gsar_rvl/RVL_ASA_WS_20091116195940116.gsar'
-        call_command('ingest_sar_doppler', f, stdout=out)
-        self.assertIn('Successfully added:', out.getvalue())
-
+        gsar_file_src = '/mnt/10.11.12.232/sat_downloads_asar/level-0/2010-01/gsar_rvl/RVL_ASA_WS_20100110111600123.gsar'
+        call_command('ingest_sar_doppler', gsar_file_src, epsg=4326, reprocess=False, stdout=out)
+        # self.assertIsInstance(out['epsg'], int)
+        # self.assertEqual(())
+        output = out.getvalue()
+        self.assertIsInstance(output['epsg'], int)
