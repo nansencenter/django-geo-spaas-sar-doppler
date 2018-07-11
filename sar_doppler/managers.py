0c2b5527226dd1c199885d582d3613e60384f353
Artem Moiseev <korvinos@bk.ru>
2018-07-11 14:46:47 +0200

    #6 Working version

--- sar_doppler/managers.py
+++ sar_doppler/managers.py
@@ -31,125 +31,48 @@ class DatasetManager(DM):
     NUM_SUBSWATS = 5
     NUM_BORDER_POINTS = 10
 
-    def list_of_coordinates(self, left, right, upper, lower, axis):
-        coord_list = np.concatenate(
-            (left[axis][0], upper[axis][0], upper[axis][1], upper[axis][2],
-             upper[axis][3], upper[axis][4], np.flipud(right[axis][4]),
-             np.flipud(lower[axis][4]), np.flipud(lower[axis][3]),
-             np.flipud(lower[axis][2]), np.flipud(lower[axis][1]),
-             np.flipud(lower[axis][0])))
-
-        return coord_list
-
-    def update_borders(self, swath_data, i, step, border):
-        """
-        Add information about borders coordinates and steps size from the swath
-        :param swath_data: sardoppler.sardoppler.Doppler object, Data from one swath
-        :param i: int, number of swath
-        :param step: dict, Dictionary for accumulation of maximal steps in azimuthal and range direction
-        :param border: dict, Dictionary for accumulation of lat/con coordinates for each border;
-        left and right are azimuthal borders; upper and bottom are range borders
-        :return: dict, dict, <step> and <border> dictionaries updated with values from the input swath
-        """
-        lon, lat = swath_data.get_geolocation_grids()
-
-        step['azimuth'][i] = max(1, (lon.shape[0] / 2 * 2 - 1) / self.NUM_BORDER_POINTS)
-        step['range'][i] = max(1, (lon.shape[1] / 2 * 2 - 1) / self.NUM_BORDER_POINTS)
-
-        border['left']['lon'][i] = lon[0:-1:step['azimuth'][i], 0]
-        border['left']['lat'][i] = lat[0:-1:step['azimuth'][i], 0]
-
-        border['right']['lon'][i] = lon[0:-1:step['azimuth'][i], -1]
-        border['right']['lat'][i] = lat[0:-1:step['azimuth'][i], -1]
-
-        border['upper']['lon'][i] = lon[-1, 0:-1:step['range'][i]]
-        border['upper']['lat'][i] = lat[-1, 0:-1:step['range'][i]]
-
-        border['lower']['lon'][i] = lon[0, 0:-1:step['range'][i]]
-        border['lower']['lat'][i] = lat[0, 0:-1:step['range'][i]]
-
-        return step, border
-
     def get_or_create(self, uri, srs, extent_dict, reprocess=False, *args, **kwargs):
+
+        # Assemble domain
+        spec_domain = False
+        if srs and extent_dict:
+            extent_str = DatasetManager.assemble_domain_extent(extent_dict)
+            dom = Domain(srs, extent_str)
+            spec_domain = True
+
+        filename = nansat_filename(uri)
+        image_geometry = DatasetManager.unite_geometry(filename, self.NUM_SUBSWATS)
+
+        if spec_domain:
+            intersection = DatasetManager.check_intersection(dom, image_geometry)
+            if not intersection:
+                return None, True
 
         # ingest file to db
         ds, created = super(DatasetManager, self).get_or_create(uri, *args, **kwargs)
         if not type(ds) == Dataset:
             return ds, False
 
-        # Assemble domain
-        if srs and extent_dict:
-            extent_str = DatasetManager.assemble_domain_extent(extent_dict)
-            dom = Domain(srs, extent_str)
-            spec_domain = True
-
         # set Dataset entry_title
         ds.entry_title = 'SAR Doppler'
         ds.save()
+        not_corrupted = True
 
-        fn = nansat_filename(uri)
-        n = Nansat(fn, subswath=0)
+        n = Nansat(filename, subswath=0)
         gg = WKTReader().read(n.get_border_wkt())
         if ds.geographic_location.geometry.area > gg.area and not reprocess:
             return ds, False
-
-        ''' Update dataset border geometry
-
-        This must be done every time a Doppler file is processed. It is time
-        consuming but apparently the only way to do it. Could be checked
-        though...
-        '''
-        swath_data = {}
-
-        steps = {
-            'azimuth': {},
-            'range': {}
-        }
-        # Dictionaries for accumulation of image border coordinates
-
-        borders = {
-            'left': {'lat': {}, 'lon': {}},  # Azimuthal direction
-            'right': {'lat': {}, 'lon': {}},  # Azimuthal direction
-            'upper': {'lat': {}, 'lon': {}},  # Range direction
-            'lower': {'lat': {}, 'lon': {}}  # Range direction
-        }
-
-        # Flag for detection of corruption in img
-        not_corrupted = True
-
-        for i in range(self.NUM_SUBSWATS):
-            # Read subswath
-            swath_data[i] = Nansat(fn, subswath=i)
-
-            # Should use nansat.domain.get_border - see nansat issue #166
-            # (https://github.com/nansencenter/nansat/issues/166)
-            steps, borders = self.update_borders(swath_data[i], i, steps, borders)
-
-        lons = self.list_of_coordinates(borders['left'], borders['right'],
-                                        borders['upper'], borders['lower'], 'lon')
-        # apply 180 degree correction to longitude - code copied from
-        # get_border_wkt...
-
-        # TODO: This loop returns exactly the same list of lons
-        for ilon, llo in enumerate(lons):
-            lons[ilon] = copysign(acos(cos(llo * pi / 180.)) / pi * 180, sin(llo * pi / 180.))
-
-        lats = self.list_of_coordinates(borders['left'], borders['right'],
-                                        borders['upper'], borders['lower'], 'lat')
-
-        # Create a polygon form lats and lons
-        new_geometry = Polygon(zip(lons, lats))
 
         # Get geolocation of dataset - this must be updated
         geoloc = ds.geographic_location
 
         # Check geometry, return if it is the same as the stored one
-        if geoloc.geometry == new_geometry and not reprocess:
+        if geoloc.geometry == image_geometry.ExportToIsoWkt() and not reprocess:
             return ds, True
 
-        if geoloc.geometry != new_geometry:
+        if geoloc.geometry != image_geometry.ExportToIsoWkt():
             # Change the dataset geolocation to cover all subswaths
-            geoloc.geometry = new_geometry
+            geoloc.geometry = image_geometry.ExportToIsoWkt()
             geoloc.save()
 
         # Create data products
@@ -186,3 +109,7 @@ class DatasetManager(DM):
             n = Nansat(uri, subswath=subswath_num)
             poly_base = poly_base.Union(n.get_border_geometry())
         return poly_base
+
+    @staticmethod
+    def check_intersection(dom, img_geometry):
+        return img_geometry.Intersect(dom.get_border_geometry())
--- sar_doppler/tests.py
+++ sar_doppler/tests.py
@@ -1,9 +1,78 @@
 from django.test import TestCase
 from management.commands.ingest_sar_doppler import Command as IngestCommand
 from django.core.management.base import CommandError
+from django.core.management import call_command
+from django.utils.six import StringIO
+from managers import DatasetManager
+from geospaas.utils import nansat_filename
+from unittest import skip
+from geospaas.catalog.models import Dataset
+from nansat import Domain
+
+
+class TestDatasetManager(TestCase):
+    fixtures = ["vocabularies"]
+
+    gsar_file_src = 'file://localhost/mnt/10.11.12.232/sat_downloads_asar/' \
+                    'level-0/2010-01/gsar_rvl/RVL_ASA_WS_20100110111600123.gsar'
+
+    def test_check_intersection(self):
+        # Domain inside image
+        dom_1 = Domain(4326, '-lle 3 73.5 3.2 74 -tr 0.1 0.1')
+        # Domain intersect border of image
+        dom_2 = Domain(4326, '-lle 3 73.5 6 74 -tr 0.1 0.1')
+        # Image inside the domain
+        dom_3 = Domain(4326, '-lle -6 70 6 75 -tr 0.1 0.1')
+        # Image outside of the domain
+        dom_4 = Domain(4326, '-lle 6 70 10 75 -tr 0.1 0.1')
+
+        geometry = DatasetManager.unite_geometry(nansat_filename(self.gsar_file_src), 2)
+        self.assertTrue(DatasetManager.check_intersection(dom_1, geometry))
+        self.assertTrue(DatasetManager.check_intersection(dom_2, geometry))
+        self.assertTrue(DatasetManager.check_intersection(dom_3, geometry))
+        self.assertFalse(DatasetManager.check_intersection(dom_4, geometry))
+
+    def test_assemble_domain_extent(self):
+        extent_1 = {'lle': [1, 2, 3, 4], 'ts': [1, 2]}
+        extent_2 = {'te': [1.1, 2, -3, 4], 'tr': [1, 0.2]}
+
+        extent = DatasetManager.assemble_domain_extent(extent_1)
+        self.assertIsInstance(extent, str)
+        self.assertEqual(extent, '-lle 1 2 3 4 -ts 1 2')
+        extent = DatasetManager.assemble_domain_extent(extent_2)
+        self.assertEqual(extent, '-te 1.1 2 -3 4 -tr 1 0.2')
+
+    @skip("It is almost matching. Difference is ~0.000000001")
+    def test_unite_geometry(self):
+        test_geometry = 'MULTIPOLYGON (((4.77975239848 74.1678231767,4.22945358468 73.8507101827,3.6945098806 73.5327174445,3.17492128623 73.2138449623,2.67068780159 72.8940927361,2.18180942668 72.5734607659,1.70828616148 72.2519490515,1.250118006 71.9295575932,0.807304960248 71.6062863908,0.379847024214 71.2821354443,-0.0527999489107 70.9405542659,-0.422843440109 70.9889200031,-0.780991005562 71.0350550034,-1.12724264527 71.0789592666,-1.46159835923 71.1206327928,-1.78405814744 71.1600755821,-2.09462200991 71.1972876343,-2.39328994663 71.2322689496,-2.68006195761 71.2650195279,-2.95493804284 71.2955393692,-3.26059168057 71.328326447,-2.88696264788 71.6771344009,-2.51566694549 72.0081624402,-2.12901613337 72.3383107354,-1.72701021153 72.6675792866,-1.30964917998 72.9959680938,-0.876933038696 73.3234771569,-0.428861787695 73.6501064759,0.0345645730268 73.9758560509,0.51334604347 74.3007258819,1.00748262363 74.6247159688,1.37791243025 74.5839970176,1.70831094596 74.5466784277,2.05060538742 74.5071291007,2.40479575462 74.4653490368,2.77088204756 74.421338236,3.14886426625 74.3750966981,3.53874241069 74.3266244232,3.94051648087 74.2759214114,4.3541864768 74.2229876626,4.77975239848 74.1678231767)),((0.869877202765 74.6434163981,0.478234846317 74.3847006495,0.0959996792631 74.1255334281,-0.276828298397 73.8659147337,-0.640249086664 73.6058445663,-0.994262685538 73.345322926,-1.33886909502 73.0843498128,-1.6740683151 72.8229252267,-1.9998603458 72.5610491676,-2.3162451871 72.2987216355,-2.6369528507 72.0239874032,-2.85091557295 72.0472810756,-3.0621242675 72.070010278,-3.27057893437 72.0921750102,-3.47627957354 72.1137752723,-3.67922618504 72.1348110643,-3.87941876884 72.1552823862,-4.07685732495 72.175189238,-4.27154185338 72.1945316197,-4.46347235412 72.2133095313,-4.66605604868 72.2328023333,-4.37708929988 72.5105900951,-4.09106533361 72.7758383944,-3.79563417795 73.0406352209,-3.4907958329 73.3049805743,-3.17655029846 73.5688744549,-2.85289757462 73.8323168625,-2.51983766138 74.0953077972,-2.17737055876 74.3578472589,-1.82549626674 74.6199352477,-1.46421478533 74.8815717635,-1.2291854748 74.8589576387,-1.00697239915 74.8372664923,-0.782005295822 74.8150108759,-0.554284164802 74.7921907894,-0.323809006094 74.7688062328,-0.0905798196977 74.7448572061,0.145403394386 74.7203437092,0.384140636158 74.6952657423,0.625631905617 74.6696233052,0.869877202765 74.6434163981)))'
+        geometry = DatasetManager.unite_geometry(nansat_filename(self.gsar_file_src), 2)
+        self.assertEqual(geometry.ExportToIsoWkt(), test_geometry)
 
 
 class TestIngestCommand(TestCase):
+
+    fixtures = ["vocabularies"]
+
+    gsar_file_src = 'file://localhost/mnt/10.11.12.232/sat_downloads_asar/' \
+                    'level-0/2010-01/gsar_rvl/RVL_ASA_WS_20100110111600123.gsar'
+
+    def test_command_options_1(self):
+
+        out = StringIO()
+        call_command('ingest_sar_doppler', self.gsar_file_src,
+                     epsg=4326, lle=[3, 73.5, 3.2, 74], tr=[0.1, 0.1], with_domain=True,
+                     reprocess=False, stdout=out)
+        ds = Dataset.objects.all()
+        self.assertEqual(len(ds), 1)
+
+    def test_command_options_2(self):
+
+        out = StringIO()
+        call_command('ingest_sar_doppler', self.gsar_file_src,
+                     epsg=4326, lle=[6, 70, 10, 75], tr=[0.1, 0.1], with_domain=True,
+                     reprocess=False, stdout=out)
+        ds = Dataset.objects.all()
+        self.assertEqual(len(ds), 0)
 
     def test_check_srs(self):
         options_1 = {'epsg': None, 'proj4': None}
@@ -83,9 +152,6 @@ class TestIngestCommand(TestCase):
         self.assertEqual(extent['te'], [1, 2, 3, 4])
         self.assertEqual(extent['ts'], [1, 2])
 
-from django.core.management import call_command
-from django.utils.six import StringIO
-from managers import DatasetManager
 
 # class TestProcessingSARDoppler(TestCase):
 #
@@ -100,20 +166,5 @@ from managers import DatasetManager
 #                'gsar_rvl/RVL_ASA_WS_20091116195940116.gsar'
 #        call_command('ingest_sar_doppler', f, stdout=out)
 #        self.assertIn('Successfully added:', out.getvalue())
-
-
-#class TestIngestSARDopplerCommand(TestCase):
-#    fixtures = ["vocabularies"]
-#
-#    gsar_file_src = 'file://localhost/mnt/10.11.12.232/sat_downloads_asar/' \
-#                    'level-0/2010-01/gsar_rvl/RVL_ASA_WS_20100110111600123.gsar'
-#
-#    def test_command_options(self):
-#        out = StringIO()
-#        call_command('ingest_sar_doppler', self.gsar_file_src,
-#                     epsg=4326, reprocess=False, stdout=out)
-#
-#    def test_domain(self):
-#        pass
 
 
