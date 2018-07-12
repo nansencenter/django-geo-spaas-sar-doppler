f95fca22c575308cbc71b9752b72951209350ce3
Artem Moiseev <korvinos@bk.ru>
2018-07-12 15:00:43 +0200

    #10 Update tests

--- sar_doppler/tests.py
+++ sar_doppler/tests.py
@@ -1,13 +1,47 @@
 from django.test import TestCase
+
 from management.commands.ingest_sar_doppler import Command as IngestCommand
+from managers import DatasetManager
+from models import Dataset as SARDAtaset
 from django.core.management.base import CommandError
 from django.core.management import call_command
 from django.utils.six import StringIO
-from managers import DatasetManager
 from geospaas.utils import nansat_filename
 from unittest import skip
 from geospaas.catalog.models import Dataset
 from nansat import Domain
+
+
+class TestDataset(TestCase):
+    fixtures = ["vocabularies"]
+
+    gsar_file_src = 'file://localhost/mnt/10.11.12.232/sat_downloads_asar/' \
+                    'level-0/2010-01/gsar_rvl/RVL_ASA_WS_20100110111600123.gsar'
+    srs = 4326
+    extent = {'lle': [3, 73.5, 6, 74], 'tr': [0.1, 0.1]}
+
+    def setUp(self):
+        self.ds, _ = SARDAtaset.objects.get_or_create(self.gsar_file_src, self.srs, self.extent)
+
+    def test_file_added2db(self):
+        self.assertEqual(len(Dataset.objects.all()), 1)
+
+    def test_time_coverage(self):
+        self.assertIsInstance(self.ds.time_coverage_start, str)
+        self.assertEqual(self.ds.time_coverage_start, '2010-01-10T11:15:59.021842')
+        self.assertIsInstance(self.ds.time_coverage_end, str)
+        self.assertEqual(self.ds.time_coverage_end, '2010-01-10T11:17:00.465423')
+
+    @skip('future')
+    def test_polarization(self):
+        pass
+
+    @skip('future')
+    def test_path(self):
+        pass
+
+    def test_uri(self):
+        self.assertEqual(self.ds.dataseturi_set.first().uri, self.gsar_file_src)
 
 
 class TestDatasetManager(TestCase):
@@ -47,6 +81,44 @@ class TestDatasetManager(TestCase):
         test_geometry = 'MULTIPOLYGON (((4.77975239848 74.1678231767,4.22945358468 73.8507101827,3.6945098806 73.5327174445,3.17492128623 73.2138449623,2.67068780159 72.8940927361,2.18180942668 72.5734607659,1.70828616148 72.2519490515,1.250118006 71.9295575932,0.807304960248 71.6062863908,0.379847024214 71.2821354443,-0.0527999489107 70.9405542659,-0.422843440109 70.9889200031,-0.780991005562 71.0350550034,-1.12724264527 71.0789592666,-1.46159835923 71.1206327928,-1.78405814744 71.1600755821,-2.09462200991 71.1972876343,-2.39328994663 71.2322689496,-2.68006195761 71.2650195279,-2.95493804284 71.2955393692,-3.26059168057 71.328326447,-2.88696264788 71.6771344009,-2.51566694549 72.0081624402,-2.12901613337 72.3383107354,-1.72701021153 72.6675792866,-1.30964917998 72.9959680938,-0.876933038696 73.3234771569,-0.428861787695 73.6501064759,0.0345645730268 73.9758560509,0.51334604347 74.3007258819,1.00748262363 74.6247159688,1.37791243025 74.5839970176,1.70831094596 74.5466784277,2.05060538742 74.5071291007,2.40479575462 74.4653490368,2.77088204756 74.421338236,3.14886426625 74.3750966981,3.53874241069 74.3266244232,3.94051648087 74.2759214114,4.3541864768 74.2229876626,4.77975239848 74.1678231767)),((0.869877202765 74.6434163981,0.478234846317 74.3847006495,0.0959996792631 74.1255334281,-0.276828298397 73.8659147337,-0.640249086664 73.6058445663,-0.994262685538 73.345322926,-1.33886909502 73.0843498128,-1.6740683151 72.8229252267,-1.9998603458 72.5610491676,-2.3162451871 72.2987216355,-2.6369528507 72.0239874032,-2.85091557295 72.0472810756,-3.0621242675 72.070010278,-3.27057893437 72.0921750102,-3.47627957354 72.1137752723,-3.67922618504 72.1348110643,-3.87941876884 72.1552823862,-4.07685732495 72.175189238,-4.27154185338 72.1945316197,-4.46347235412 72.2133095313,-4.66605604868 72.2328023333,-4.37708929988 72.5105900951,-4.09106533361 72.7758383944,-3.79563417795 73.0406352209,-3.4907958329 73.3049805743,-3.17655029846 73.5688744549,-2.85289757462 73.8323168625,-2.51983766138 74.0953077972,-2.17737055876 74.3578472589,-1.82549626674 74.6199352477,-1.46421478533 74.8815717635,-1.2291854748 74.8589576387,-1.00697239915 74.8372664923,-0.782005295822 74.8150108759,-0.554284164802 74.7921907894,-0.323809006094 74.7688062328,-0.0905798196977 74.7448572061,0.145403394386 74.7203437092,0.384140636158 74.6952657423,0.625631905617 74.6696233052,0.869877202765 74.6434163981)))'
         geometry = DatasetManager.unite_geometry(nansat_filename(self.gsar_file_src), 2)
         self.assertEqual(geometry.ExportToIsoWkt(), test_geometry)
+
+    def test_get_pass_from_uri(self):
+        uri1 = '/path/to/ascending/HH/gsar_rvl/file.gsar'
+        uri2 = '/path/to/descending/HH/gsar_rvl/file.gsar'
+        uri3 = '/path/to/no_pass_info/HH/gsar_rvl/file.gsar'
+        uri4 = '/path/to/descendingHH/gsar_rvl/file.gsar'
+
+        sat_pass = DatasetManager.get_pass_from_uri(uri1)
+        self.assertIsInstance(sat_pass, str)
+        self.assertEqual(sat_pass, 'ascending')
+
+        sat_pass = DatasetManager.get_pass_from_uri(uri2)
+        self.assertEqual(sat_pass, 'descending')
+
+        sat_pass = DatasetManager.get_pass_from_uri(uri3)
+        self.assertIsNone(sat_pass)
+
+        sat_pass = DatasetManager.get_pass_from_uri(uri4)
+        self.assertIsNone(sat_pass)
+
+    def test_get_pol_from_uri(self):
+        uri1 = '/path/to/ascending/HH/gsar_rvl/file.gsar'
+        uri2 = '/path/to/descending/VV/gsar_rvl/file.gsar'
+        uri3 = '/path/to/no_pass_info/no_pol_info/gsar_rvl/file.gsar'
+        uri4 = '/path/to/descendingHH/gsar_rvl/file.gsar'
+
+        sat_pass = DatasetManager.get_pol_from_uri(uri1)
+        self.assertIsInstance(sat_pass, str)
+        self.assertEqual(sat_pass, 'HH')
+
+        sat_pass = DatasetManager.get_pol_from_uri(uri2)
+        self.assertEqual(sat_pass, 'VV')
+
+        sat_pass = DatasetManager.get_pol_from_uri(uri3)
+        self.assertIsNone(sat_pass)
+
+        sat_pass = DatasetManager.get_pol_from_uri(uri4)
+        self.assertIsNone(sat_pass)
 
 
 class TestIngestCommand(TestCase):
