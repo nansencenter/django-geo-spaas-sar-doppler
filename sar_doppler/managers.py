bd9c61838f4f1f154e0501a58fe5de959775fb8e
Artem Moiseev <korvinos@bk.ru>
2018-07-20 09:55:40 +0200

    fix uri in manager and remove reprocessing

--- sar_doppler/managers.py
+++ sar_doppler/managers.py
@@ -12,49 +12,6 @@ from sardoppler.sardoppler import Doppler
 from datetime import datetime
 
 
-class DatasetReprocessing:
-
-    DOMAIN = 'file://localhost'
-    WKV_NAME = {
-        'dc_anomaly': 'anomaly_of_surface_backwards_doppler_centroid_frequency_shift_of_radar_wave',
-        'dc_velocity': 'surface_backwards_doppler_frequency_shift_of_radar_wave_due_to_surface_velocity'
-    }
-
-    def reprocess(self, uri, srs, extent_dict, reprocess=False, *args, **kwargs):
-        # map(lambda i: self.generate_product(filename, i, ds), range(self.NUM_SUBSWATS))
-        pass
-
-    def generate_product(self, uri, swath_num, dataset):
-        swath_data = Doppler(uri, subswath=swath_num)
-        swath_data.add_band(array=swath_data.anomaly(),
-                            parameters={'wkv': self.WKV_NAME['dc_anomaly']})
-        swath_data.add_band(array=swath_data.geophysical_doppler_shift(),
-                            parameters={'wkv': self.WKV_NAME['dc_velocity']})
-
-        self.export(swath_data, swath_num, dataset)
-
-    def export(self, swath_data, swath_num, dataset):
-        ppath = self.get_product_path(swath_data.fileName)
-        file_dst = DatasetManager.assemble_filename(ppath, swath_data.fileName, swath_num)
-        print('Exporting: %s' % file_dst)
-        swath_data.set_metadata(key='Originating file', value=swath_data.fileName)
-        swath_data.export(filename=file_dst)
-        ncuri = os.path.join(self.DOMAIN, file_dst)
-        new_uri, created = DatasetURI.objects.get_or_create(uri=ncuri, dataset=dataset)
-
-    @staticmethod
-    def assemble_filename(ppath, origin,  swath_num):
-        basename = os.path.basename(origin).split('.')[0]
-        return '%s/%ssubswath%d.nc' % (ppath, basename, swath_num)
-
-    def get_product_path(self, origin):
-        mm = self.__module__.split('.')
-        module = '%s.%s' % (mm[0], mm[1])
-        # local uri path for visualizations
-        ppath = product_path(module, origin)
-        return ppath
-
-
 class DatasetManager(models.Manager):
 
     NUM_SUBSWATS = 5
@@ -100,6 +57,7 @@ class DatasetManager(models.Manager):
             polarization=DatasetManager.get_pol_from_uri(uri))
 
         ds.save()
+        _ = DatasetURI.objects.get_or_create(uri=uri, dataset=ds)[0]
         not_corrupted = True
 
         n = Nansat(filename, subswath=0)
